1f1fa8685095e3c633c242f96108e99875e87aee
Пейко Олег Петрович <pop457045@hotmail.com>
2023-05-26 20:38:35 +0300

    Lets Start

--- ChatHost/HostForm.cs
+++ ChatHost/HostForm.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using ChatHost.ServiceChat;
-using EasyWcfChat;
 
 namespace ChatHost
 {
--- EasyWcfChat/ServiceChat.cs
+++ EasyWcfChat/ServiceChat.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
 
 namespace EasyWcfChat
 {
